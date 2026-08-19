187a04488266202b0ee6b2b0ba8d1edf75a1ac72
ihsankhan-752 <ihsan@example.com>
2026-08-19 22:24:26 +0500

    Authentication Validation Added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,39 @@
 import { User } from "../models/user.model.js";
+import asyncHandler from "../utils/async.handler.js";
+import ErrorHandler from "../utils/error.handler.js";
+import { signUpSchema } from "../validations/auth.validation.js";
 
+export const userSignUp = asyncHandler(async (req, res, next) => {
+  const validationResult = signUpSchema.safeParse(req.body);
 
+  if (!validationResult.success) {
+    const message = validationResult.error.errors[0].message;
+    return next(new ErrorHandler(message, 400));
+  }
 
+  const { email, firstName, lastName, password } = validationResult.data;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return next(new ErrorHandler("User Already exist", 409));
+  }
+
+  const user = await User.create({
+    email,
+    firstName,
+    lastName,
+    password,
+  });
+
+  return res.status(201).json({
+    success: true,
+    message: "User Account Created",
+    user: {
+      _id: user._id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: user.role,
+    },
+  });
+});
---
+++ validations/auth.validation.js
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  email: z
+    .string({ required_error: "Email required" })
+    .trim()
+    .email("Invalid Email format")
+    .toLowerCase(),
+
+  firstName: z
+    .string({ required_error: "First Name required" })
+    .trim()
+    .min(2, "First Name Must be atleast 2 characters"),
+
+  lastName: z
+    .string({ required_error: "Last Name required" })
+    .trim()
+    .min(2, "Last Name must be atleast 2 characters"),
+
+  password: z
+    .string({ required_error: "Password Required" })
+    .min(6, "Password must be at least 6 characters"),
+});
+
+
+
